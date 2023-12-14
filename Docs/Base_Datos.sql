8b5c14620dbcb98a7cc92a56ca4beb084bece1ff
kevinpaf <kevinpaf@gmail.com>
2023-12-14 17:08:15 -0500

    base de datos actualizada

--- Docs/Base_Datos.sql
+++ Docs/Base_Datos.sql
@@ -1,4 +1,78 @@
-CREATE TABLE `Usuarios` (
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 14-12-2023 a las 23:06:36
+-- Versión del servidor: 10.4.24-MariaDB
+-- Versión de PHP: 7.4.29
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `sexto`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `hospitales`
+--
+
+CREATE TABLE `hospitales` (
+  `id_hospital` int(11) NOT NULL,
+  `nombre` varchar(255) NOT NULL,
+  `ciudad` varchar(255) NOT NULL,
+  `numero_camas` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `hospitales`
+--
+
+INSERT INTO `hospitales` (`id_hospital`, `nombre`, `ciudad`, `numero_camas`) VALUES
+(4, 'patate', 'patate', 10),
+(6, 'pelileo', 'pelileo', 30),
+(7, 'patate2', 'dfb', 10);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `pacientes`
+--
+
+CREATE TABLE `pacientes` (
+  `id_paciente` int(11) NOT NULL,
+  `id_hospital` int(11) DEFAULT NULL,
+  `nombre` varchar(255) NOT NULL,
+  `especialidad` varchar(255) NOT NULL,
+  `numero_cama` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `pacientes`
+--
+
+INSERT INTO `pacientes` (`id_paciente`, `id_hospital`, `nombre`, `especialidad`, `numero_cama`) VALUES
+(1, 4, 'kevin pablo', 'rayos', 5),
+(3, 6, 'hola', 'cardiologia', 5),
+(5, 4, 'pablo', 'rayos', 31);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuarios`
+--
+
+CREATE TABLE `usuarios` (
   `UsuarioId` int(11) NOT NULL,
   `Cedula` varchar(17) NOT NULL,
   `Nombres` varchar(100) NOT NULL,
@@ -10,13 +84,35 @@ CREATE TABLE `Usuarios` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
+-- Volcado de datos para la tabla `usuarios`
+--
+
+INSERT INTO `usuarios` (`UsuarioId`, `Cedula`, `Nombres`, `Apellidos`, `Telefono`, `Correo`, `Contrasenia`, `Rol`) VALUES
+(1, '1804810586', 'Kevin', 'alulema', '0123456789', 'kevinpaf@gmail.com', 'kevin', 'Administrador'),
+(2, '12345', '23456', 'QW3E4R5TYU', '123456', '1234', '1234', 'Vendedor'),
+(16, '1806206502', 'manuel', 'fonseca', '958592929', 'fetomoo@gmail.com', '12345', 'Cliente');
+
+--
 -- Índices para tablas volcadas
 --
 
 --
--- Indices de la tabla `Usuarios`
+-- Indices de la tabla `hospitales`
 --
-ALTER TABLE `Usuarios`
+ALTER TABLE `hospitales`
+  ADD PRIMARY KEY (`id_hospital`);
+
+--
+-- Indices de la tabla `pacientes`
+--
+ALTER TABLE `pacientes`
+  ADD PRIMARY KEY (`id_paciente`),
+  ADD KEY `hospitales_pacientes` (`id_hospital`);
+
+--
+-- Indices de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
   ADD PRIMARY KEY (`UsuarioId`);
 
 --
@@ -24,8 +120,34 @@ ALTER TABLE `Usuarios`
 --
 
 --
--- AUTO_INCREMENT de la tabla `Usuarios`
+-- AUTO_INCREMENT de la tabla `hospitales`
 --
-ALTER TABLE `Usuarios`
-  MODIFY `UsuarioId` int(11) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `hospitales`
+  MODIFY `id_hospital` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- AUTO_INCREMENT de la tabla `pacientes`
+--
+ALTER TABLE `pacientes`
+  MODIFY `id_paciente` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  MODIFY `UsuarioId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `pacientes`
+--
+ALTER TABLE `pacientes`
+  ADD CONSTRAINT `hospitales_pacientes` FOREIGN KEY (`id_hospital`) REFERENCES `hospitales` (`id_hospital`) ON DELETE CASCADE;
 COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
